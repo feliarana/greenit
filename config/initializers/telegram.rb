248a36d01cd68128e50cc52d1936f9c9828ea6a5
Felipe Arana <felipe.arana@aero.tur.ar>
2020-07-03 18:34:28 -0300

    saco sqlite para subir a heroku

--- config/initializers/telegram.rb
+++ config/initializers/telegram.rb
@@ -5,5 +5,5 @@ Telegram.bots_config = {
 
 Telegram.bot.get_updates
 Telegram.bot == Telegram.bots[:default] # true
-Telegram.bots[:default].send_message('HELLO WORLD!')
+# Telegram.bots[:default].send_message('HELLO WORLD!')
 # Telegram.bot.get_meTelegram.bots[:default]].send_message('HELLO WORLD!')
